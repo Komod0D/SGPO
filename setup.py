826f9cff3e3cb173422ad81e0c0d73bdd9f7b5ae
John Doe <kimonroxd@gmail.com>
2026-02-04 16:16:30 +0100

    restructure

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name='sgpo',
+      version='1.0',
+      description='Steered Generation for Protein Optimization (SGPO)',
+      author='misc',
+      packages=find_packages(include=['sgpo', 'sgpo.*']),
+      include_package_data=True
+     )
